55a9a8ff60295d401f339ab7bd7a21e0c57d04ad
Parv <parvg555@gmail.com>
2021-09-19 17:18:41 +0530

    made double motor function

--- Simulation/sketch_sep19a/sketch_sep19a.ino
+++ Simulation/sketch_sep19a/sketch_sep19a.ino
@@ -7,6 +7,9 @@ int enable = 8;
 int motorx = 1;                 // This is the motor number for x-axis movement
 int motory = 2;                 // This is the motor number for y-axis movement
 
+int directionx_multiplier = 1;  //set this -1 for counter rotation
+int directiony_multiplier = 1;	// set this -1 for counter rotation 
+
 //////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
@@ -42,6 +45,43 @@ void rotate(int motor,int steps,int dxn){
     	delayMicroseconds(1290);
   	}
   	delay(1000);
+}
+
+void RotateBothMotors(int motor1, int motor2, int steps, int dir1, int dir2){
+	int direction_pin1, direction_pin2;
+	int stepper_pin1, stepper_pin2;
+	
+	if(motor1 == 1){
+		direction_pin1 = dirPin1;
+		stepper_pin1 = stepPin1;
+		direction_pin2 = dirPin2;
+		stepper_pin2 = stepPin2; 
+	}else{
+		direction_pin1 = dirPin2;
+		stepper_pin1 = stepPin2;
+		direction_pin2 = dirPin1;
+		stepper_pin2 = stepPin1;
+	}
+
+	if(dir1 == 1){
+		digitalWrite(direction_pin1,HIGH);
+	}else{
+		digitalWrite(direction_pin1,low);
+	}
+	if(dir2 == 1){
+		digitalWrite(direction_pin2,HIGH);
+	}else{
+		digitalWrite(direction_pin2,LOW);
+	}
+	for(int i=0;i<steps;i++){
+		digitalWrite(stepper_pin1,HIGH);
+		digitalWrite(stepper_pin2,HIGH);
+		delayMicroseconds(10);
+		digitalWrite(stepper_pin1,LOW);
+		digitalWrite(stepper_pin2,LOW);
+		delayMicroseconds(1290);
+	}
+	delay(1000);
 }
 
 // New rotate function to make motors move together 
@@ -86,7 +126,7 @@ void ul(){
 
 }
 
-void loop() {
+void loop(){
     rotate(1,200,1); // motor - 1  steps - 200 direction - clockwise
     rotate(2,200,1);
     rotate(1,200,2);
